0d9e0c2482974fbba82cfd112d05bee7815045ab
cyberdeeb <abrahamdeeb@gmail.com>
2024-11-16 09:22:44 -0800

    pygame screen set up

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import constants
 import pygame
 
 def main():
-    print('Starting asteroids!')
-    print(f'Screen width: {constants.SCREEN_WIDTH}')
-    print(f'Screen height: {constants.SCREEN_HEIGHT}')
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill('black')
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
